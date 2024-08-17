b2aef8cac571d113f89eb53c18915a0ba7ed18f6
Andrew Masek <andrewmasek@gmail.com>
2024-08-16 21:51:43 -0700

    Added support for new extractor types & implemented ListExtractor and ItemExtractor

--- parsera/engine/simple_extractor.py
+++ parsera/engine/simple_extractor.py
@@ -1,5 +1,4 @@
 import json
-
 from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import JsonOutputParser
@@ -106,3 +105,80 @@ Output json:
 
 class TabularExtractor(JsonExtractor):
     system_prompt = TABULAR_EXTRACTOR_SYSTEM_PROMPT
+
+
+LIST_EXTRACTOR_SYSTEM_PROMPT = """
+Your goal is to find the elements from the webpage content and return them in json format.
+For example if user asks:
+Return the following elements from the page content:
+```
+{
+    "name": "name of the listing",
+    "price": "price of the listing"
+}
+```
+Make sure to return json with the list of corresponding values.
+Output json:
+```json
+{
+    "name": ["name1", "name2", "name3"],
+    "price": "100", "150", "300"]
+}
+```
+
+If users asks for a single field:
+Return the following elements from the page content:
+```
+{
+    "link": "link to the listing",
+}
+```
+Make sure to return json with only this field
+Output json:
+```json
+{
+    "link": ["https://example.com/link1", "https://example.com/link2", "https://example.com/link3"]
+}
+```
+
+"""
+class ListExtractor(JsonExtractor):
+    system_prompt = LIST_EXTRACTOR_SYSTEM_PROMPT
+
+ITEM_EXTRACTOR_SYSTEM_PROMPT = """
+Your goal is to find the elements from the webpage content and return them in json format.
+For example if user asks:
+Return the following elements from the page content:
+```
+{
+    "name": "name of the listing",
+    "price": "price of the listing"
+}
+```
+Make sure to return a json with corresponding values.
+Output json:
+```json
+{
+    "name": "name1",
+    "price": "100"
+}
+```
+
+If users asks for a single field:
+Return the following elements from the page content:
+```
+{
+    "link": "link to the listing",
+}
+```
+Make sure to return json with only this field
+Output json:
+```json
+{
+    "link": "https://example.com/link1"]
+}
+```
+
+"""
+class ItemExtractor(JsonExtractor):
+    system_prompt = ITEM_EXTRACTOR_SYSTEM_PROMPT
--- parsera/main.py
+++ parsera/main.py
@@ -1,18 +1,23 @@
 import asyncio
-
 from langchain_core.language_models import BaseChatModel
-
+import enum
 from parsera.engine.model import GPT4oMiniModel
-from parsera.engine.simple_extractor import TabularExtractor
+from parsera.engine.simple_extractor import TabularExtractor, ListExtractor, ItemExtractor
 from parsera.page import fetch_page_content
 
 
 class Parsera:
-    def __init__(self, model: BaseChatModel | None = None):
+    class ExtractorType(enum.Enum):
+        LIST = ListExtractor
+        TABULAR = TabularExtractor
+        ITEM = ItemExtractor
+
+    def __init__(self, model: BaseChatModel | None = None, extractor: ExtractorType = ExtractorType.TABULAR):
         if model is None:
             self.model = GPT4oMiniModel()
         else:
             self.model = model
+        self.extractor = extractor
 
     async def _run(
         self, url: str, elements: dict, proxy_settings: dict | None = None
@@ -21,10 +26,10 @@ class Parsera:
             content = await fetch_page_content(url=url, proxy_settings=proxy_settings)
         else:
             content = await fetch_page_content(url=url)
-        extractor = TabularExtractor(
+        extractor_instance = self.extractor.value(
             elements=elements, model=self.model, content=content
         )
-        result = await extractor.run()
+        result = await extractor_instance.run()
         return result
 
     def run(self, url: str, elements: dict, proxy_settings: dict | None = None) -> dict:
