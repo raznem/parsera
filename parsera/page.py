6a6db8a52e6e1dbb006ccb425ffdb41313588a11
Mikhail Zanka <raznem@gmail.com>
2024-08-27 17:56:20 +0200

    PageLoader class implementation

--- parsera/page.py
+++ parsera/page.py
@@ -1,6 +1,13 @@
-from typing import TypedDict
+import warnings
+from typing import Literal, TypedDict
 
-from playwright.async_api import async_playwright
+from playwright.async_api import (
+    Browser,
+    BrowserContext,
+    Page,
+    Playwright,
+    async_playwright,
+)
 from playwright_stealth import stealth_async
 
 
@@ -11,19 +18,73 @@ class ProxySettings(TypedDict, total=False):
     password: str | None = None
 
 
+class PageLoader:
+    def __init__(
+        self,
+        browser: Literal["firefox", "chromium"] = "firefox",
+    ):
+        self._browser_id = browser
+        self.playwright: Playwright | None = None
+        self.browser: Browser | None = None
+        self.context: BrowserContext | None = None
+        self.page: Page | None = None
+
+    async def new_browser(self) -> None:
+        if not self.playwright:
+            self.playwright = await async_playwright().start()
+
+        if self.browser:
+            await self.browser.close()
+
+        if self._browser_id == "firefox":
+            self.browser = await self.playwright.firefox.launch(headless=True)
+        else:
+            self.browser = await self.playwright.chromium.launch(headless=True)
+
+    async def load_content(
+        self,
+        url: str,
+        proxy_settings: ProxySettings | None = None,
+        new_browser: bool = True,
+        load_state: Literal[
+            "domcontentloaded", "load", "networkidle"
+        ] = "domcontentloaded",
+    ) -> None:
+        if new_browser:
+            await self.new_browser()
+        self.context = await self.browser.new_context(proxy=proxy_settings)
+
+        self.page = await self.context.new_page()
+        await stealth_async(self.page)
+        # Navigate to the URL
+        # await page.route("**/*.{png,jpg,jpeg}", lambda route: route.abort()) # Can speed up requests
+        await self.page.goto(url)
+        await self.page.wait_for_load_state(load_state)
+        return await self.page.content()
+
+    async def close(self) -> None:
+        if self.playwright:
+            await self.browser.close()
+            self.playwright.stop()
+
+
 async def fetch_page_content(
     url: str,
     proxy_settings: ProxySettings | None = None,
     browser: str = "firefox",
 ) -> str:
+    warnings.warn(
+        "fetch_page_content is deprecated and will be removed",
+        DeprecationWarning,
+    )
     async with async_playwright() as p:
         # Launch the browser
         if browser == "firefox":
-            browser = await p.firefox.launch(headless=True, proxy=proxy_settings)
+            browser = await p.firefox.launch(headless=True)
         else:
-            browser = await p.chromium.launch(headless=True, proxy=proxy_settings)
+            browser = await p.chromium.launch(headless=True)
         # Open a new browser context
-        context = await browser.new_context()
+        context = await browser.new_context(proxy=proxy_settings)
         # Open a new page
         page = await context.new_page()
         await stealth_async(page)
