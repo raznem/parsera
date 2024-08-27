7936e8e64923e5e2cde1b0cb2eba06fa9598110b
Mikhail Zanka <raznem@gmail.com>
2024-08-27 17:56:20 +0200

    PageLoader class implementation

--- parsera/main.py
+++ parsera/main.py
@@ -1,9 +1,16 @@
 import asyncio
-from langchain_core.language_models import BaseChatModel
 import enum
+
+from langchain_core.language_models import BaseChatModel
+
 from parsera.engine.model import GPT4oMiniModel
-from parsera.engine.simple_extractor import TabularExtractor, ListExtractor, ItemExtractor
-from parsera.page import fetch_page_content
+from parsera.engine.simple_extractor import (
+    ItemExtractor,
+    ListExtractor,
+    TabularExtractor,
+)
+from parsera.page import PageLoader, fetch_page_content
+
 
 class Parsera:
     class ExtractorType(enum.Enum):
@@ -11,20 +18,22 @@ class Parsera:
         TABULAR = TabularExtractor
         ITEM = ItemExtractor
 
-    def __init__(self, model: BaseChatModel | None = None, extractor: ExtractorType = ExtractorType.TABULAR):
+    def __init__(
+        self,
+        model: BaseChatModel | None = None,
+        extractor: ExtractorType = ExtractorType.TABULAR,
+    ):
         if model is None:
             self.model = GPT4oMiniModel()
         else:
             self.model = model
         self.extractor = extractor
+        self.loader = PageLoader()
 
     async def _run(
         self, url: str, elements: dict, proxy_settings: dict | None = None
     ) -> dict:
-        if proxy_settings:
-            content = await fetch_page_content(url=url, proxy_settings=proxy_settings)
-        else:
-            content = await fetch_page_content(url=url)
+        content = await self.loader.load_content(url=url, proxy_settings=proxy_settings)
         extractor_instance = self.extractor.value(
             elements=elements, model=self.model, content=content
         )
